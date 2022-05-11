f1cc3c4cbb3241cd1e68994b535281ba3c9beca9
German Hornus <hornusgerman@gmail.com>
2022-05-11 00:36:45 -0300

    add api fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
+import { useEffect, useState } from 'react';
+import { SearchTrendingMovies } from './lib/api/SearchTrendingMovies';
+
 const App = () => {
+	const { movies, page, error, loading, setPage } = useMovies();
+	if (loading) return <h1>Cargando...</h1>;
+	if (error) return <p>{error}</p>;
 	return (
 		<div>
-			<h1 className='text-4xl text-blue-600 font-bold '>Hello world!</h1>
+			<button onClick={() => setPage(page + 1)}>Página: {page}</button>
+			{movies && movies.map(movie => <div key={movie.id}>{movie.title}</div>)}
 		</div>
 	);
+};
+
+const searchTrending = async (page, setMovies, setError, setLoading) => {
+	setLoading(true);
+	const { success, data, statusCode } = await SearchTrendingMovies(page);
+	if (success) setMovies(data.movies);
+	else {
+		setMovies();
+		setError(`Error: ${statusCode}`);
+	}
+	setLoading(false);
+};
+
+const useMovies = () => {
+	const [movies, setMovies] = useState([]);
+	const [page, setPage] = useState(1);
+	const [error, setError] = useState();
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		searchTrending(page, setMovies, setError, setLoading);
+	}, [page]);
+	return { movies, page, error, loading, setPage };
 };
 
 export default App;
